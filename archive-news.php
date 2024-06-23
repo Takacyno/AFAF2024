18fb9256ff547be709ed84067bb2e348682b8915
shimizu1219 <atsuyoshi.shimizu@tdrk-inc.co.jp>
2024-06-23 18:37:20 +0900

    News一覧にサイドメニュー追加.

--- archive-news.php
+++ archive-news.php
@@ -2,59 +2,68 @@
 
 <main class="p-news">
 	<div class="c-area__content">
-		<div class="c-title">
-			<h1>NEWS</h1>
-		</div>
-		<?php
-			$term = get_queried_object();
-			$paged = get_query_var('paged') ?: 1;
-			$args = array(
-				'post_type' => array('news'), /* 投稿タイプを指定 */
-				'paged' => $paged,
-				'orderby' => 'menu_order',
-				'order' => 'DESC',
-				'posts_per_page' => '-1' /* 20件を取得 */
-			);
-		?>
-		<ul class="p-news__list">
-		<?php setup_postdata( $args ); ?>
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<li class="p-news__list-item">
+		<div class="p-news__detail">
+		<div class="p-news__detail-side">
+				<div class="p-news__detail-back">
+					<a href="<?php echo home_url('top'); ?>"><span>Top</span></a>
+				</div>
+			</div>
+			<div class="p-news__detail-main">
+				<div class="c-title">
+					<h1>NEWS</h1>
+				</div>
 				<?php
-					$year = get_the_time('Y');
-					$month = get_the_time('m');
-					$date = get_the_time('d');
-					$monthArray = [
-						"01" => "JAN",
-						"02" => "FEB",
-						"03" => "MAR",
-						"04" => "APR",
-						"05" => "MAY",
-						"06" => "JUN",
-						"07" => "JUL",
-						"08" => "AUG",
-						"09" => "SEP",
-						"10" => "OCT",
-						"11" => "NOV",
-						"12" => "DEC",
-					];
-					$url = get_field( 'url' );
-					if ( $url ) :
-					// 外部リンクに飛ばす場合
+					$term = get_queried_object();
+					$paged = get_query_var('paged') ?: 1;
+					$args = array(
+						'post_type' => array('news'), /* 投稿タイプを指定 */
+						'paged' => $paged,
+						'orderby' => 'menu_order',
+						'order' => 'DESC',
+						'posts_per_page' => '-1' /* 20件を取得 */
+					);
 				?>
-					<a href="<?php echo $url; ?>" target="_blank">
-						<span class="p-news__list-date"><?php echo $monthArray[$month] .' ' . $date . ', ' . $year; ?></span>
-						<span class="p-news__list-title"><?php the_title(); ?></span>
-					</a>
-				<?php else: ?>
-					<a href="<?php echo the_permalink( $post->ID ); ?>">
-						<span class="p-news__list-date"><?php echo $monthArray[$month] .' ' . $date . ', ' . $year; ?></span>
-						<span class="p-news__list-title"><?php the_title(); ?></span>
-					</a>
-				<?php endif; ?>
-			</li>
-		<?php endwhile; endif; ?>
-		</ul>
+				<ul class="p-news__list">
+				<?php setup_postdata( $args ); ?>
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<li class="p-news__list-item">
+						<?php
+							$year = get_the_time('Y');
+							$month = get_the_time('m');
+							$date = get_the_time('d');
+							$monthArray = [
+								"01" => "JAN",
+								"02" => "FEB",
+								"03" => "MAR",
+								"04" => "APR",
+								"05" => "MAY",
+								"06" => "JUN",
+								"07" => "JUL",
+								"08" => "AUG",
+								"09" => "SEP",
+								"10" => "OCT",
+								"11" => "NOV",
+								"12" => "DEC",
+							];
+							$url = get_field( 'url' );
+							if ( $url ) :
+							// 外部リンクに飛ばす場合
+						?>
+							<a href="<?php echo $url; ?>" target="_blank">
+								<span class="p-news__list-date"><?php echo $monthArray[$month] .' ' . $date . ', ' . $year; ?></span>
+								<span class="p-news__list-title"><?php the_title(); ?></span>
+							</a>
+						<?php else: ?>
+							<a href="<?php echo the_permalink( $post->ID ); ?>">
+								<span class="p-news__list-date"><?php echo $monthArray[$month] .' ' . $date . ', ' . $year; ?></span>
+								<span class="p-news__list-title"><?php the_title(); ?></span>
+							</a>
+						<?php endif; ?>
+					</li>
+				<?php endwhile; endif; ?>
+				</ul>
+			</div>
+		</div>
 	</div>
 </main>
 
