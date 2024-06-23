41a6bdad9911bba412e7baeeb2f94dfb52a8a91f
shimizu1219 <atsuyoshi.shimizu@tdrk-inc.co.jp>
2024-06-23 18:34:14 +0900

    Exhibitors一覧サイドメニュー追加.

--- archive-exhibitors.php
+++ archive-exhibitors.php
@@ -63,129 +63,138 @@
 
 <main class="p-exhibitors">
 	<div class="c-area__content">
-		<div class="c-title">
-			<h1>EXHIBITORS</h1>
-		</div>
-		<!-- セクション区切りのborder -->
-		<!-- <div class="p-exhibitors__section js-fadein"> -->
-		<div class="p-exhibitors__section">
-			<div id="exhibitorsHeading" class="p-exhibitors__section-heading">
-				<ul class="p-exhibitors__section-tab">
-					<li><button type="button" aria-controls="all" aria-selected="true">All</button></li>
-					<?php
-						for ($i = 0; $i < $exhibitorsTermNum; $i++):
-					?>
-						<li><button type="button" aria-controls="<?php echo $exhibitorsTermSlugs[$i]; ?>" aria-selected="false"><?php echo $exhibitorsTermNames[$i]; ?></button></li>
-					<?php endfor; ?>
-				</ul>
+		<div class="p-exhibitors__detail">
+		<div class="p-exhibitors__detail-side">
+				<div class="p-exhibitors__detail-back">
+					<a href="<?php echo home_url('top'); ?>"><span>Top</span></a>
+				</div>
 			</div>
-			<div class="p-exhibitors__section-content">
-				<div class="p-exhibitors__section-tabpanel">
-					<div id="all" class="p-exhibitors__section-tabpanel-all" aria-hidden="false">
-						<ul class="p-exhibitors__taxonomy">
+			<div class="p-exhibitors__detail-main">
+				<div class="c-title">
+					<h1>EXHIBITORS</h1>
+				</div>
+				<!-- セクション区切りのborder -->
+				<!-- <div class="p-exhibitors__section js-fadein"> -->
+				<div class="p-exhibitors__section">
+					<div id="exhibitorsHeading" class="p-exhibitors__section-heading">
+						<ul class="p-exhibitors__section-tab">
+							<li><button type="button" aria-controls="all" aria-selected="true">All</button></li>
 							<?php
-								$args=array(
-									'post_type' => 'exhibitors', //カスタム投稿名
-									'order' => 'ASC',
-									'posts_per_page'=> -1 //表示件数（-1で全ての記事を表示）
-								);
-								query_posts( $args );
-								if(have_posts()):
-									$i2 = 0;
-									while(have_posts()):the_post();
-										$i2++;
-										$number = get_field( 'number' );
+								for ($i = 0; $i < $exhibitorsTermNum; $i++):
 							?>
-							<li class="p-exhibitors__taxonomy-item">
-								<a href="<?php echo the_permalink( $post->ID ); ?>">
-									<h3><?php the_title(); ?></h3>
-									<?php
-										$section = get_the_terms($post -> ID, 'section');
-										if ( $section ) {
-											foreach($section as $term) {
-												$sectionName = $term->name;
-												echo '<span class="p-exhibitors__taxonomy-item-location">' . $sectionName . '</span>';
-											}
-										}
-									?>
-									<?php /*
-									<dl>
-										<dt>Booth</dt>
-										<dd><?php echo $number; ?></dd>
-									</dl>
-									*/ ?>
-								</a>
-							</li>
-							<?php
-								endwhile;
-								endif;
-							?>
+								<li><button type="button" aria-controls="<?php echo $exhibitorsTermSlugs[$i]; ?>" aria-selected="false"><?php echo $exhibitorsTermNames[$i]; ?></button></li>
+							<?php endfor; ?>
 						</ul>
 					</div>
-					<?php
-						wp_reset_postdata();
-						// allここまで
-					?>
+					<div class="p-exhibitors__section-content">
+						<div class="p-exhibitors__section-tabpanel">
+							<div id="all" class="p-exhibitors__section-tabpanel-all" aria-hidden="false">
+								<ul class="p-exhibitors__taxonomy">
+									<?php
+										$args=array(
+											'post_type' => 'exhibitors', //カスタム投稿名
+											'order' => 'ASC',
+											'posts_per_page'=> -1 //表示件数（-1で全ての記事を表示）
+										);
+										query_posts( $args );
+										if(have_posts()):
+											$i2 = 0;
+											while(have_posts()):the_post();
+												$i2++;
+												$number = get_field( 'number' );
+									?>
+									<li class="p-exhibitors__taxonomy-item">
+										<a href="<?php echo the_permalink( $post->ID ); ?>">
+											<h3><?php the_title(); ?></h3>
+											<?php
+												$section = get_the_terms($post -> ID, 'section');
+												if ( $section ) {
+													foreach($section as $term) {
+														$sectionName = $term->name;
+														echo '<span class="p-exhibitors__taxonomy-item-location">' . $sectionName . '</span>';
+													}
+												}
+											?>
+											<?php /*
+											<dl>
+												<dt>Booth</dt>
+												<dd><?php echo $number; ?></dd>
+											</dl>
+											*/ ?>
+										</a>
+									</li>
+									<?php
+										endwhile;
+										endif;
+									?>
+								</ul>
+							</div>
+							<?php
+								wp_reset_postdata();
+								// allここまで
+							?>
 
-					<?php
-						for ($i = 0; $i < $exhibitorsTermNum; $i++):
-					?>
-					<div id="<?php echo $exhibitorsTermSlugs[$i]; ?>" class="p-exhibitors__section-tabpanel-item" aria-hidden="true">
-						<ul class="p-exhibitors__taxonomy">
 							<?php
-								$args=array(
-									'post_type' => 'exhibitors', //カスタム投稿名
-									'order' => 'ASC',
-									'tax_query' => array(
-										array(
-											'taxonomy' => 'section',
-											'field' => 'slug',
-											'terms' => array($exhibitorsTermSlugs[$i]),
-											'_locale' => $currentLang,
-										)
-									),
-									'posts_per_page'=> -1 //表示件数（-1で全ての記事を表示）
-								);
-								query_posts( $args );
-								if(have_posts()):
-									$i2 = 0;
-									while(have_posts()):the_post();
-										$i2++;
-										$number = get_field( 'number' );
+								for ($i = 0; $i < $exhibitorsTermNum; $i++):
 							?>
-							<li class="p-exhibitors__taxonomy-item">
-								<a href="<?php echo the_permalink( $post->ID ); ?>">
-									<h3><?php the_title(); ?></h3>
+							<div id="<?php echo $exhibitorsTermSlugs[$i]; ?>" class="p-exhibitors__section-tabpanel-item" aria-hidden="true">
+								<ul class="p-exhibitors__taxonomy">
 									<?php
-										$section = get_the_terms($post -> ID, 'section');
-										if ( $section ) {
-											foreach($section as $term) {
-												$sectionName = $term->name;
-												echo '<span class="p-exhibitors__taxonomy-item-location">' . $sectionName . '</span>';
-											}
-										}
+										$args=array(
+											'post_type' => 'exhibitors', //カスタム投稿名
+											'order' => 'ASC',
+											'tax_query' => array(
+												array(
+													'taxonomy' => 'section',
+													'field' => 'slug',
+													'terms' => array($exhibitorsTermSlugs[$i]),
+													'_locale' => $currentLang,
+												)
+											),
+											'posts_per_page'=> -1 //表示件数（-1で全ての記事を表示）
+										);
+										query_posts( $args );
+										if(have_posts()):
+											$i2 = 0;
+											while(have_posts()):the_post();
+												$i2++;
+												$number = get_field( 'number' );
 									?>
-									<?php /*
-									<dl>
-										<dt>Booth</dt>
-										<dd><?php echo $number; ?></dd>
-									</dl>
-									*/ ?>
-								</a>
-							</li>
+									<li class="p-exhibitors__taxonomy-item">
+										<a href="<?php echo the_permalink( $post->ID ); ?>">
+											<h3><?php the_title(); ?></h3>
+											<?php
+												$section = get_the_terms($post -> ID, 'section');
+												if ( $section ) {
+													foreach($section as $term) {
+														$sectionName = $term->name;
+														echo '<span class="p-exhibitors__taxonomy-item-location">' . $sectionName . '</span>';
+													}
+												}
+											?>
+											<?php /*
+											<dl>
+												<dt>Booth</dt>
+												<dd><?php echo $number; ?></dd>
+											</dl>
+											*/ ?>
+										</a>
+									</li>
+									<?php
+										endwhile;
+										endif;
+									?>
+								</ul>
+							</div>
 							<?php
-								endwhile;
-								endif;
+								endfor;
+								wp_reset_postdata();
 							?>
-						</ul>
 					</div>
-					<?php
-						endfor;
-						wp_reset_postdata();
-					?>
 				</div>
 			</div>
 		</div>
+	</div>
 </main>
 
 <?php get_footer(); ?>
