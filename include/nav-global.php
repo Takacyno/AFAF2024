575ec7990119aaf9fdb4584417b05cd099939902
shimizu1219 <atsuyoshi.shimizu@tdrk-inc.co.jp>
2024-06-25 23:57:13 +0900

    HeaderのProgramsをコメントアウト.

--- include/nav-global.php
+++ include/nav-global.php
@@ -6,14 +6,14 @@ $globalnav_on_value = get_post_meta($program_page_id, 'globalnav_on', true);
 ?>
 <div class="c-nav-global">
 	<ul>
-		<li><a href="<?php echo home_url('about'); ?>">ABOUT</a></li>
 		<li><a href="<?php echo home_url('news'); ?>">NEWS</a></li>
 		<li><a href="<?php echo home_url('exhibitors'); ?>">EXHIBITORS</a></li>
+		<li><a href="<?php echo home_url('about'); ?>">ABOUT</a></li>
 		<li><a href="https://art-scenes.net/ja/fairs/31/artworks" target="_blank">ARTWORKS</a></li>
-		<?php if ($globalnav_on_value) : ?>
+		<?php /* if ($globalnav_on_value) : ?>
 		<li><a href="<?php echo home_url('program'); ?>">PROGRAM</a></li>
-		<?php endif; ?>
-		<li><a href="<?php echo home_url('access'); ?>">ACCESS</a></li>
+		<?php endif; */ ?>
+		<li><a href="<?php echo home_url('access'); ?>">VISIT</a></li>
 		<li><a href="<?php echo home_url('partners'); ?>">PARTNERS</a></li>
 	</ul>
 	<div class="c-nav-global__vip">
