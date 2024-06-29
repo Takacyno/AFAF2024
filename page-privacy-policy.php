e10ea7e44e4a69dbb2449fe3c749b1b74c0cfb2b
shimizu1219 <atsuyoshi.shimizu@tdrk-inc.co.jp>
2024-06-29 22:19:54 +0900

    privacu-policyページを調整.

--- page-privacy-policy.php
+++ page-privacy-policy.php
@@ -4,7 +4,7 @@
 	<div class="c-area__content">
 		<div class="c-area__content-inner">
 			<div class="p-privacy__wrapper">
-				<div class="c-area__content-main">
+				<div class="c-area__content-main margin-right">
 					<div class="c-title">
 						<h1><?php the_title(); ?></h1>
 					</div>
