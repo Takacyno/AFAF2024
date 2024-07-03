e08c49741c9364279201559978057042a5befe91
shimizu1219 <atsuyoshi.shimizu@tdrk-inc.co.jp>
2024-07-03 17:11:28 +0900

    archivesページを調整.

--- page-archives.php
+++ page-archives.php
@@ -1,55 +1,59 @@
 <?php include('header.php'); ?>
 
 <main class="p-archives">
-	<div class="c-title" data-title="minimal">
-		<h1><?php the_title(); ?></h1>
-	</div>
 	<div class="c-area__content">
-		<div class="p-press__wrapper">
-				<div class="p-press__section js-fadein">
-					<div class="p-press__section-heading">
-						<h2>CLOSING REPORT</h2>
-					</div>
-					<div class="p-press__list">
-						<?php
-							if(have_rows('closing')):
-						?>
-							<ul>
-								<?php
-									while(have_rows('closing')): the_row();
-										$title = get_sub_field('title');
-										$pdf = get_sub_field('pdf');
-								?>
-									<li><a href="<?php echo $pdf; ?>" target="_blank"><span><?php echo $title ?></span></a></li>
-								<?php endwhile; ?>
-							</ul>
-						<?php endif; ?>
-					</div>
+		<div class="c-area__content-inner">
+			<div class="c-area__content-main">
+				<div class="c-title" data-title="minimal">
+					<h1><?php the_title(); ?></h1>
 				</div>
-				<div class="p-press__section js-fadein">
-					<div class="p-press__section-heading">
-						<h2>VR</h2>
-					</div>
-					<div class="p-archives__vr">
-						<h3>2022</h3>
-						<?php
-							if(have_rows('vr')):
-						?>
-							<ul>
+				<div class="p-archive__wrapper">
+						<div class="p-archive__section">
+							<div class="p-archive__section-heading">
+								<h2>Closing Report</h2>
+							</div>
+							<div class="p-archive__list">
 								<?php
-									while(have_rows('vr')): the_row();
-										$url = get_sub_field('url');
-										$year = get_sub_field('year');
+									if(have_rows('closing')):
 								?>
-									<div class="p-archives__vr-item">
-										<div class="p-archives__vr-iframe">
-											<iframe src="<?php echo $url; ?>" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-										</div>
-										<a href="<?php echo $url; ?>">View Full Screen</a>
-									</div>
-								<?php endwhile; ?>
-							</ul>
-						<?php endif; ?>
+									<ul>
+										<?php
+											while(have_rows('closing')): the_row();
+												$title = get_sub_field('title');
+												$pdf = get_sub_field('pdf');
+										?>
+											<li><a href="<?php echo $pdf; ?>" target="_blank"><span><?php echo $title ?></span></a></li>
+										<?php endwhile; ?>
+									</ul>
+								<?php endif; ?>
+							</div>
+						</div>
+						<div class="p-archive__section">
+							<div class="p-archive__section-heading">
+								<h2>VR</h2>
+							</div>
+							<div class="p-archives__vr">
+								<h3>2022</h3>
+								<?php
+									if(have_rows('vr')):
+								?>
+									<ul>
+										<?php
+											while(have_rows('vr')): the_row();
+												$url = get_sub_field('url');
+												$year = get_sub_field('year');
+										?>
+											<div class="p-archives__vr-item">
+												<div class="p-archives__vr-iframe">
+													<iframe src="<?php echo $url; ?>" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+												</div>
+												<a href="<?php echo $url; ?>">View Full Screen</a>
+											</div>
+										<?php endwhile; ?>
+									</ul>
+								<?php endif; ?>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
