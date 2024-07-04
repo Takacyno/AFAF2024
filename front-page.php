ffb4c2f26b1e24bf96001d109a0344b22af1f15b
shimizu1219 <atsuyoshi.shimizu@tdrk-inc.co.jp>
2024-07-04 14:58:05 +0900

    topページのdetailをコメントアウト.

--- front-page.php
+++ front-page.php
@@ -240,9 +240,11 @@
 								</li>
 							</ul>
 						</div>
+						<!--
 						<div class="c-link">
 							<a href="<?php echo home_url('access'); ?>"><span>Detail</span></a>
 						</div>
+						-->
 					</div>
 				</div>
 			</div>
