720f0805a1be824f5cd6deb9f9646f9619545359
shimizu1219 <atsuyoshi.shimizu@tdrk-inc.co.jp>
2024-07-11 12:19:51 +0900

    partner英語ページを修正.

--- page-partners.php
+++ page-partners.php
@@ -11,12 +11,6 @@
 					<h1><?php the_title(); ?></h1>
 				</div>
 				<div class="p-partners__wrapper">
-					<?php
-						if (have_posts()) :
-							while (have_posts()) :
-								the_post();
-								if (empty(get_the_content())) :
-					?>
 					<div class="p-partners__content">
 						<?php
 							if(have_rows('co_sponsored_repeat')):
@@ -213,11 +207,6 @@
 							</div>
 						<?php endif; ?>
 					</div>
-					<?php else: ?>
-					<div class="p-partners__content">
-						<?php the_content(); ?>
-					</div>
-					<?php endif; endwhile; endif; ?>
 				</div>
 			</div>
 		</div>
