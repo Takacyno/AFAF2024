16d0d96162066afdf40dc137f444a533fb4aef4a
shimizu1219 <atsuyoshi.shimizu@tdrk-inc.co.jp>
2024-07-27 17:38:31 +0900

    vipプログラム詳細ページの調整.

--- single-vip-program.php
+++ single-vip-program.php
@@ -9,75 +9,25 @@
 
 <main class="p-vip" data-news="detail">
 	<div class="c-area__content">
-		<div class="p-vip__detail">
-			<div class="p-vip__detail-side">
-				<div class="p-vip__detail-back">
-					<a href="<?php echo home_url('vip'); ?>"><span>ALL ARTICLES</span></a>
+		<div class="c-area__content-inner">
+			<div class="c-area__content-side">
+				<div class="c-area__content-back">
+					<a href="<?php echo home_url('vip'); ?>"><span>VIP-PROGRAM</span></a>
 				</div>
-				<div class="p-vip__detail-meta">
-					<?php if ($location) : ?>
-					<dl>
-						<div>
-							<?php if ($currentLang != 'en_US') : ?>
-							<dt>会場</dt>
-							<?php else: ?>
-							<dt>VENUE</dt>
-							<?php endif; ?>
-							<dd><?php echo $location; ?></dd>
-						</div>
-					</dl>
-					<?php
-						endif;
-						$section = get_the_terms($post -> ID, 'vip-program-section');
-						if ( $section ) :
-					?>
-					<ul class="p-vip__detail-taxonomy">
-						<?php
-							foreach($section as $term) {
-								$sectionName = $term->name;
-								echo '<li class="p-vip__detail-taxonomy-item">' . $sectionName . '</li>';
-							}
-						?>
-					</ul>
-					<?php endif; ?>
-				</div>
-				<div class="p-vip__detail-pager" data-pager="pc">
-					<?php
-						$prevpost = get_previous_post();
-						$nextpost = get_next_post();
-						if ( $nextpost ):
-					?>
-						<a href="<?php echo the_permalink($nextpost->ID); ?>" data-link="prev">
-							<span>PREV</span>
-						</a>
-						<?php
-							$nextNextpost = get_next_post($nextpost->ID);
-							if ( $nextNextpost ):
-						?>
-						<a href="<?php echo the_permalink($nextNextpost->ID); ?>" data-link="prev">
-							<span>PREV</span>
-						</a>
-						<?php endif; ?>
-					<?php
-						endif;
-						if ( $prevpost ):
-					?>
-						<a href="<?php echo the_permalink($prevpost->ID); ?>" data-link="next">
-							<span>NEXT</span>
-						</a>
-						<?php
-							$prevPrevpost = get_previous_post($prevpost->ID);
-							if ( $prevPrevpost ):
-						?>
-							<a href="<?php echo the_permalink($prevPrevpost->ID); ?>" data-link="prev">
-								<span>PREV</span>
-							</a>
-						<?php endif; ?>
+				<div class="p-vip__aply-side">
+					<?php if ($currentLang != 'en_US') : ?>
+					<a href="#" class="p-vip__aply">お申し込みはこちら</a>
+					<?php else: ?>
+					<a href="#" class="p-vip__aply">Click here to apply</a>
 					<?php endif; ?>
 				</div>
 			</div>
-			<div class="p-vip__detail-main">
+			<div class="c-area__content-main margin-right">
 				<div class="p-vip__detail-header">
+					<h1><?php the_title(); ?></h1>
+				</div>
+				<?php // header ここまで ?>
+				<div class="p-vip__detail-content">
 					<?php
 						$dates = get_field( 'dates' );
 						$hours = get_field( 'hours' );
@@ -94,10 +44,29 @@
 						<span class="p-vip__detail-header-hours"><?php echo $hours; ?></span>
 						<?php endif; ?>
 					</div>
-					<h1><?php the_title(); ?></h1>
-				</div>
-				<?php // header ここまで ?>
-				<div class="p-vip__detail-content">
+					<?php if ($location) : ?>
+					<dl class="p-vip__detail-venue">
+						<?php if ($currentLang != 'en_US') : ?>
+						<dt>会場:</dt>
+						<?php else: ?>
+						<dt>VENUE:</dt>
+						<?php endif; ?>
+						<dd><?php echo $location; ?></dd>
+					</dl>
+					<?php
+						endif;
+						$section = get_the_terms($post -> ID, 'vip-program-section');
+						if ( $section ) :
+					?>
+					<ul class="p-vip__detail-taxonomy">
+						<?php
+							foreach($section as $term) {
+								$sectionName = $term->name;
+								echo '<li class="p-vip__detail-taxonomy-item">' . $sectionName . '</li>';
+							}
+						?>
+					</ul>
+					<?php endif; ?>
 					<?php
 						if (has_post_thumbnail()):
 					?>
@@ -106,41 +75,14 @@
 					</div>
 					<?php endif; ?>
 					<?php echo $contents; ?>
+					<div class="ly__center">
+						<?php if ($currentLang != 'en_US') : ?>
+						<a href="#" class="p-vip__aply padding-x-115">お申し込みはこちら</a>
+						<?php else: ?>
+						<a href="#" class="p-vip__aply padding-x-115">Click here to apply</a>
+						<?php endif; ?>
+					<div>
 				</div>
-			</div>
-			<div class="p-vip__detail-pager" data-pager="sp">
-				<?php
-					$prevpost = get_previous_post();
-					$nextpost = get_next_post();
-					if ( $nextpost ):
-				?>
-					<a href="<?php echo the_permalink($nextpost->ID); ?>" data-link="prev">
-						<span>PREV</span>
-					</a>
-					<?php
-						$nextNextpost = get_next_post($nextpost->ID);
-						if ( $nextNextpost ):
-					?>
-					<a href="<?php echo the_permalink($nextNextpost->ID); ?>" data-link="prev">
-						<span>PREV</span>
-					</a>
-					<?php endif; ?>
-				<?php
-					endif;
-					if ( $prevpost ):
-				?>
-					<a href="<?php echo the_permalink($prevpost->ID); ?>" data-link="next">
-						<span>NEXT</span>
-					</a>
-					<?php
-						$prevPrevpost = get_previous_post($prevpost->ID);
-						if ( $prevPrevpost ):
-					?>
-						<a href="<?php echo the_permalink($prevPrevpost->ID); ?>" data-link="prev">
-							<span>PREV</span>
-						</a>
-					<?php endif; ?>
-				<?php endif; ?>
 			</div>
 		</div>
 	</div>
